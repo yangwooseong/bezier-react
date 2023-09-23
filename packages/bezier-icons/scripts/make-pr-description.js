8927cbacc98d3c0162978f4174c9c6206257c678
andrew <andrew@channel.io>
2023-09-23 12:35:15 +0900

    chore: add log

--- packages/bezier-icons/scripts/make-pr-description.js
+++ packages/bezier-icons/scripts/make-pr-description.js
@@ -1,7 +1,9 @@
 const { exec } = require('child_process')
 
-const githubToken = process.argv[0]
-const pullNumber = process.argv[1]
+const githubToken = process.argv[2]
+const pullNumber = process.argv[3]
+console.log('LOG: ', process.argv)
+console.log('LOG: ', pullNumber)
 
 const keyToHeader = {
   M: 'Modified 🖊️\n',
@@ -43,14 +45,19 @@ const getDescription = gitLog => {
 exec('git log -1 --name-status --pretty="format:"', async (_undefined, stdout) => {
   const description = getDescription(stdout)
 
-  await fetch(`https://api.github.com/repos/yangwooseong/bezier-react/${pullNumber}`, {
-    method: 'PATCH',
-    headers: {
-      'X-GitHub-Api-Version': '2022-11-28',
-      Authorization: `Bearer ${githubToken}`,
-      Accept: 'application/vnd.github+json',
-    },
-    body: description,
-  })
+  try {
+    const res = await fetch(`https://api.github.com/repos/yangwooseong/bezier-react/pulls/${pullNumber}`, {
+      method: 'PATCH',
+      headers: {
+        'X-GitHub-Api-Version': '2022-11-28',
+        Authorization: `Bearer ${githubToken}`,
+        Accept: 'application/vnd.github+json',
+      },
+      body: JSON.stringify({ body: description }),
+    })
+    console.log('LOG: ', await res.json())
+  } catch (e) {
+    console.log(e)
+  }
 })
 
