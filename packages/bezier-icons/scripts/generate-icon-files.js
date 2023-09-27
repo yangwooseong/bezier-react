5709daa7ae117b40b5f16ec58fc440ead2d53ceb
andrew <andrew@channel.io>
2023-09-27 18:49:27 +0900

    feat(bezier-icons): fix to javascript because of typescript file load fail

--- packages/bezier-icons/scripts/generate-icon-files.js
+++ packages/bezier-icons/scripts/generate-icon-files.js
@@ -2,7 +2,9 @@ const path = require('path')
 
 const { generateIconFiles } = require('./utils/generateIconFilesFromJson')
 
+console.log("LOG: ", __dirname)
 const bezierIconsDirectory = path.resolve(__dirname, '..')
+console.log("LOG: ", __dirname, bezierIconsDirectory)
 const iconsJson = path.resolve(bezierIconsDirectory, 'icons.json')
 
 generateIconFiles(iconsJson, bezierIconsDirectory)
