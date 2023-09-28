bf8d066cbc29030900dfb225ba088373f4338c8d
andrew <andrew@channel.io>
2023-09-28 16:56:28 +0900

    fix: typo in yml

--- packages/bezier-icons/scripts/generate-icon-files.js
+++ packages/bezier-icons/scripts/generate-icon-files.js
@@ -1,12 +1,8 @@
 const path = require('path')
-const { execSync } = require('child_process')
 
 const { generateIconFiles } = require('./generateIconFilesFromJson')
 
-console.log("LOG: ", __dirname)
 const bezierIconsDirectory = path.resolve(__dirname, '..')
-
-console.log("LOG: ", bezierIconsDirectory)
 const iconsJson = path.resolve(bezierIconsDirectory, 'icons.json')
 
 generateIconFiles(iconsJson, bezierIconsDirectory)
