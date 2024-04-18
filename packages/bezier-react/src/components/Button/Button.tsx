db2aef6ab85692b4cbb822396115ea9db7d86706
andrew <andrew@channel.io>
2024-04-18 14:24:54 +0900

    feat: make dirty in bezier-react

--- packages/bezier-react/src/components/Button/Button.tsx
+++ packages/bezier-react/src/components/Button/Button.tsx
@@ -17,7 +17,7 @@ import styles from './Button.module.scss'
 
 export const BUTTON_TEST_ID = 'bezier-button'
 
-function getTypography_test_1(size: ButtonSize) {
+function getTypography_test_2(size: ButtonSize) {
   return (
     {
       xs: '13',
@@ -147,7 +147,7 @@ export const Button = forwardRef<HTMLButtonElement, ButtonProps>(
           {text && (
             <Text
               className={styles.ButtonText}
-              typo={getTypography_test_1(size)}
+              typo={getTypography_test_2(size)}
               bold
             >
               {text}
